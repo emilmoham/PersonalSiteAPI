f0fd77bf05e6edfe6dae88ce35331d0650afc5f4
emilmoham <emilmoham@gmail.com>
2022-01-08 13:08:10 -0500

    Testing CI

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const blogRoute = require('./routes/blog');
 // Attach Route Middlewares
 app.use('/blog', blogRoute);
 
+app.get("/", (req, res) =>{
+  res.json("Hello World!");
+})
+
 
 app.listen(8081, () => {
   console.log('Server running on port 8081')
